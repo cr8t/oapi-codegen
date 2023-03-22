e30448bdbb77977c83f63e2d4c9b422a618d6979
cr8t <124842229+cr8t@users.noreply.github.com>
2023-03-22 12:09:21 -0600

    codegen: add swaggerUriToChiPathUri helper

--- pkg/codegen/template_helpers.go
+++ pkg/codegen/template_helpers.go
@@ -284,6 +284,7 @@ var TemplateFunctions = template.FuncMap{
 	"genParamFmtString":          ReplacePathParamsWithStr,
 	"swaggerUriToEchoUri":        SwaggerUriToEchoUri,
 	"swaggerUriToChiUri":         SwaggerUriToChiUri,
+	"swaggerUriToChiPathUri":     SwaggerUriToChiPathUri,
 	"swaggerUriToGinUri":         SwaggerUriToGinUri,
 	"swaggerUriToGorillaUri":     SwaggerUriToGorillaUri,
 	"lcFirst":                    LowercaseFirstCharacter,
